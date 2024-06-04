0ef9f6a14b90d9c173d6502dd5865e434aa070a0
Charana C <charana.c@juspay.in>
2024-06-04 19:06:21 +0530

    refunds dashboard done

---
+++ pages/Refunds.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+from connection import get_connection
+
+st.set_page_config(layout="wide")
+
+# Create a connection object.
+conn = get_connection()
+df = conn.read(worksheet="Refunds" )
+st.subheader("Refunds Analytics")
+
+date_range_list = df["DateRange"].unique()
+bank_list = df['Bank'].unique()
+cols = st.columns(4)
+with cols[0]:
+    selected_date_range = st.selectbox("Select a Date range", date_range_list)
+
+with cols[1]:
+    selected_bank = st.selectbox("Select a Bank", bank_list)
+
+
+online_df = df.query('Bank == @selected_bank and DateRange == @selected_date_range and Type == "Online"')
+offline_df = df.query('Bank == @selected_bank and DateRange == @selected_date_range and Type == "Offline"')
+
+
+cols = st.columns(2)
+
+with cols[0]:
+    on_txn_fig = px.bar(online_df , x="status" , y="count" , labels={"status" : "Status" , "count" : "Count"} , text="count" , title=f"{selected_bank} Online Refunds")
+    on_txn_fig.update_layout(xaxis_title=f'{selected_bank} Online Refunds', yaxis_title='Count', title_x=0.5)
+    st.plotly_chart(on_txn_fig)
+
+with cols[1]:
+    on_sr_fig = px.bar(online_df, x="status", y="sr", labels={"sr": "Percentage (%)", "status": "Status"} ,text=["{:.1f} %".format(value) for value in online_df["sr"]] , title=f"{selected_bank} Refunds SR" )
+    on_sr_fig.update_layout(xaxis_title='Online Refunds SR', yaxis_title='Count', title_x=0.5)
+    st.plotly_chart(on_sr_fig)
+
+with cols[0]:
+    off_txn_fig = px.bar(offline_df , x="status" , y="count" , labels={"status" : "Status" , "count" : "Count"} , text="count" , title=f"{selected_bank} Offline Refunds")
+    off_txn_fig.update_layout(xaxis_title=f'{selected_bank} Offline Refunds', yaxis_title='Count', title_x=0.5)
+    st.plotly_chart(off_txn_fig)
+
+with cols[1]:
+    off_sr_fig = px.bar(offline_df, x="status", y="sr", labels={"sr": "Percentage (%)", "status": "Status"} ,text=["{:.1f} %".format(value) for value in offline_df["sr"]] , title=f"{selected_bank} Refunds SR" )
+    off_sr_fig.update_layout(xaxis_title='Offline Refunds SR', yaxis_title='Count', title_x=0.5)
+    st.plotly_chart(off_sr_fig)
---
+++ utils/upload_refunds.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd 
+from datetime import datetime
+from connection import get_connection
+
+refund_col_list = [
+    "DateRange",
+    "Bank",
+    "Type",
+    "count",
+    "status"
+]
+
+def update_refund_sheet(existing_data, new_data):
+    try:
+        # Iterate through new data
+        for _, new_row in new_data.iterrows():
+            date, bank, status , type = new_row['DateRange'], new_row['Bank'], new_row['status'] , new_row['Type']
+            
+            # Check if the date, bank, and error combination already exists
+            match = existing_data[(existing_data['DateRange'] == date) & 
+                                (existing_data['Bank'] == bank) & 
+                                (existing_data['error'] == status) & (existing_data['Type'] == type)]
+            if not match.empty:
+                # Update the 'count' for the existing row
+                existing_data.loc[match.index, 'count'] = new_row['count']
+            else:
+                # Append new row
+                existing_data = existing_data.append(new_row, ignore_index=True)
+        
+        return existing_data
+    except:
+        print("Something went wrong")
+
+
+
+def upload_error_csv(data , bank , type , start_date , end_date):
+    
+    date_range = f'{start_date} - {end_date}'
+    start_date_str, end_date_str = date_range.split(" - ")
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+    start_date_formatted = start_date.strftime("%d/%m/%Y")
+    end_date_formatted = end_date.strftime("%d/%m/%Y")
+
+    # Construct the new date range string
+    new_date_range = f"{start_date_formatted} - {end_date_formatted}"   
+    
+    data["DateRange"] = new_date_range
+    data["Bank"] = bank
+    data["Type"] = type
+    data = data.rename(columns = {'Online_volume' : 'count' , 'Online_status' : 'status'})
+    columns_to_keep_actual = [col for col in refund_col_list if col in data.columns]
+    filtered_data = data[columns_to_keep_actual]
+    
+    filtered_data['status'] = filtered_data['status'].astype(str)
+    
+    conn = get_connection()
+    client_data = conn.read(worksheet="ErrorCodes")
+    client_data.dropna(how='all', inplace=True)
+
+    updated_data = update_refund_sheet(client_data, filtered_data)
+    cre = conn.update(worksheet="Refunds" , data=updated_data)
